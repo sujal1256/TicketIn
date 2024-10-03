635b5db8a7f5b41dc78c63d5082f63304ed88737
Sujal Malhotra <msujal29012004@gmail.com>
2024-10-03 13:46:21 +0530

    Added ApiError and ApiResponse utils to send customer error and response

---
+++ backend/utils/ApiResponse.util.js
@@ -0,0 +1,9 @@
+export class ApiResponse{
+    constructor(statusCode, data, message){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+    }
+}
+
+ 
